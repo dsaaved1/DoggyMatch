1dc9f1e0d94efadb9d902c62f4e56163189bed4e
mark <mgores@iastate.edu>
2021-09-07 00:54:36 -0500

    added new activity

---
+++ Experiments/mark_gores/app/src/main/java/com/example/example/Profile.java
@@ -0,0 +1,11 @@
+package com.example.example;
+
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+public class Profile extends AppCompatActivity {
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.prof_activity);
+    }
+}
