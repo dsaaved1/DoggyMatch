c08f82b2d4cf953eafb2c81233d5af312d440947
Diego <diegos@iastate.edu>
2021-09-07 11:25:55 -0500

    adding my experiment to the main branch

---
+++ Experiments/diego/testing-Intellij/src/main/java/com/springboot/testingIntellij/TestingIntellijApplication.java
@@ -0,0 +1,15 @@
+package com.springboot.testingIntellij;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+
+@SpringBootApplication
+public class TestingIntellijApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestingIntellijApplication.class, args);
+	}
+
+}
---
+++ Experiments/diego/testing-Intellij/src/test/java/com/springboot/testingIntellij/TestingIntellijApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.testingIntellij;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestingIntellijApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
