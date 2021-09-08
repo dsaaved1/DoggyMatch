c43fce89668eaf50cc3e8c5cd3d545644a031114
mark <mgores@iastate.edu>
2021-09-07 21:49:30 -0500

    sending data between activities

--- Experiments/mark_gores/app/src/main/java/com/example/example/MainActivity.java
+++ Experiments/mark_gores/app/src/main/java/com/example/example/MainActivity.java
@@ -1,26 +1,32 @@
 package com.example.example;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
     Button log, reg;
+    EditText username;
+    String usern;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        username = (EditText) findViewById(R.id.editText1);
         log = (Button) findViewById(R.id.button);
         log.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-            profile();
+                usern = username.getText().toString();
+            profile(usern);
             }
         });
+
         reg = (Button) findViewById(R.id.button1);
         reg.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -33,8 +39,9 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-    private void profile() {
+    private void profile(String str) {
         Intent prof = new Intent(this, Profile.class);
+        prof.putExtra("message_key", str);
         startActivity(prof);
     }
     private void register(){
