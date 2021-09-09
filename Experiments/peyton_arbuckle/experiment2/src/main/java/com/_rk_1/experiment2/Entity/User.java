67123023941760c91154f9d91972775e3402a774
peyton <peyton.arbuckle@gmail.com>
2021-09-08 23:45:27 -0500

    sql experiment

--- Experiments/peyton_arbuckle/experiment2/src/main/java/com/_rk_1/experiment2/Entity/User.java
+++ Experiments/peyton_arbuckle/experiment2/src/main/java/com/_rk_1/experiment2/Entity/User.java
@@ -1,22 +1,30 @@
 package com._rk_1.experiment2.Entity;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Table;
 
-@Entity
+@Table(name = "user")
+@Entity(name = "user")
 public class User {
   @Id
   @GeneratedValue(strategy = GenerationType.AUTO)
+  @Column(name = "id")
   private Integer id;
 
+  @Column(name = "name")
   private String name;
 
+  @Column(name = "email")
   private String email;
 
+  @Column(name = "password")
   private String password;
 
+  @Column(name = "salt")
   private String salt;
 
   public String getSalt() {
--- Experiments/peyton_arbuckle/experiment2/src/main/java/com/_rk_1/experiment2/Repository/UserRepository.java
+++ Experiments/peyton_arbuckle/experiment2/src/main/java/com/_rk_1/experiment2/Repository/UserRepository.java
@@ -3,9 +3,16 @@ package com._rk_1.experiment2.Repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+import java.util.Collection;
+
 import com._rk_1.experiment2.Entity.User;
 
+@Repository("userRepository")
 public interface UserRepository extends JpaRepository<User, Integer> {
-  @Query("select u from user where u.email = ?1")
-  User findByEmail(String email);
+  @Query("SELECT u FROM user u WHERE u.email = ?1")
+  public List<User> findByEmail(String email);
 }
