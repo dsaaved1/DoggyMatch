e5244cf1273fd7575cab97748c748c5900694b1e
Corbin <cgraham1@iastate.edu>
2021-10-13 21:38:43 -0500

    Testing Functions

--- Frontend/Controller Design/HomePlate/app/src/main/java/com/example/homeplate/MainActivity.java
+++ Frontend/Controller Design/HomePlate/app/src/main/java/com/example/homeplate/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.homeplate;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,40 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        interact();
+    }
+
+    public void interact()
+    {
+        Button profileButton = findViewById(R.id.profileButton);
+        Button homeButton = findViewById(R.id.homeButton);
+        Button chatButton = findViewById(R.id.chatButton);
+
+        profileButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+
+
+            }
+        });
+
+        homeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+
+
+            }
+        });
+
+        chatButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+
+
+            }
+        });
     }
 }
