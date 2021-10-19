9944cb943476fce8757012b2718c39f37c30c69b
Corbin <cgraham1@iastate.edu>
2021-10-18 21:53:56 -0500

    Login Page

--- Frontend/Controller Design/HomePlate/app/src/main/java/com/example/homeplate/MainActivity.java
+++ Frontend/Controller Design/HomePlate/app/src/main/java/com/example/homeplate/MainActivity.java
@@ -2,50 +2,50 @@ package com.example.homeplate;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button continueButton;
+    TextView doggyTitle;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        setValues();
         interact();
     }
 
+    //Set Interaction Values
+    public void setValues()
+    {
+        //Buttons
+        continueButton = findViewById(R.id.continueButton);
+
+        //TextView
+        doggyTitle = findViewById(R.id.doggyTitle);
+
+        //TextBox
+
+    }
+
+    //Handle Interaction
     public void interact()
     {
-        Button profileButton = findViewById(R.id.profileButton);
-        Button homeButton = findViewById(R.id.homeButton);
-        Button chatButton = findViewById(R.id.chatButton);
 
-        profileButton.setOnClickListener(new View.OnClickListener() {
+        continueButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
 
-
+                doggyTitle.setText("YES!");
 
             }
         });
 
-        homeButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-
-
-
-            }
-        });
-
-        chatButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-
-
-
-            }
-        });
     }
 }
