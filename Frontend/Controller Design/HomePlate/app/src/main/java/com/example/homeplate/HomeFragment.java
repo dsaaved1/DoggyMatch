a27aabfd790d7f0076d09479a9976395eb388136
Corbin <cgraham1@iastate.edu>
2021-10-23 22:07:07 -0500

    Small Changes

--- Frontend/Controller Design/HomePlate/app/src/main/java/com/example/homeplate/HomeFragment.java
+++ Frontend/Controller Design/HomePlate/app/src/main/java/com/example/homeplate/HomeFragment.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import androidx.fragment.app.Fragment;
 
@@ -11,5 +12,8 @@ public class HomeFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_home, container, false);
+
+        
     }
+
 }
