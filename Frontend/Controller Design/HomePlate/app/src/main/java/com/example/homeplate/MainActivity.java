a82d0537bfb574b49255811ee610e9e6f753cab4
Corbin <cgraham1@iastate.edu>
2021-10-26 22:07:52 -0500

    More Features

--- Frontend/Controller Design/HomePlate/app/src/main/java/com/example/homeplate/MainActivity.java
+++ Frontend/Controller Design/HomePlate/app/src/main/java/com/example/homeplate/MainActivity.java
@@ -10,65 +10,21 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    private Button continueButton;
-    private TextView registerButton;
-    private TextView pageTitle;
-    private TextView usernameBox;
-    private TextView passwordBox;
-
-
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        setValues();
+        // setValues();
         interact();
-    }
-
-    //Set Interaction Values
-    private void setValues()
-    {
-        //Buttons
-        continueButton = findViewById(R.id.continueButton);
-        registerButton = findViewById(R.id.registerTextButton);
-        registerButton.setEnabled(false);
-
-        //TextView
-        pageTitle = findViewById(R.id.pageTitleMain);
-
-        //TextBox
-        usernameBox = findViewById(R.id.usernameBox);
-        passwordBox = findViewById(R.id.passwordBox);
     }
 
     //Handle Interaction
     private void interact()
     {
+        Intent intent = new Intent(MainActivity.this, Login.class);
+        startActivity(intent);
+        finish();
 
-        continueButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-
-                pageTitle.setText("YES!");
-
-                registerButton.setText(usernameBox.getText());
-                pageTitle.setText(passwordBox.getText());
-
-                registerButton.setEnabled(true);
-            }
-        });
-
-        registerButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-
-                // setContentView(R.layout.activity_home);
-                Intent intent = new Intent(MainActivity.this, Home.class);
-                startActivity(intent);
-                finish();
-            }
-        });
     }
 }
