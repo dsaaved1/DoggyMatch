dbc0eab599d10e18f2a3f581d409bb3bf91bf3a5
Corbin <cgraham1@iastate.edu>
2021-10-26 21:34:01 -0500

    More Features

--- Frontend/Controller Design/HomePlate/app/src/main/java/com/example/homeplate/ProfileFragment.java
+++ Frontend/Controller Design/HomePlate/app/src/main/java/com/example/homeplate/ProfileFragment.java
@@ -40,6 +40,7 @@ public class ProfileFragment extends Fragment {
             public void onClick(View view) {
                 Intent intent = new Intent(getActivity(), MainActivity.class);
                 startActivity(intent);
+                getActivity().finish();
             }
         });
     }
