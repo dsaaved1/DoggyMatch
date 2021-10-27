8e2c7985defc7278385e2e472e3cd05d77f9ecac
Corbin <cgraham1@iastate.edu>
2021-10-26 21:29:22 -0500

    More Features

--- Frontend/Controller Design/HomePlate/app/src/main/java/com/example/homeplate/ProfileFragment.java
+++ Frontend/Controller Design/HomePlate/app/src/main/java/com/example/homeplate/ProfileFragment.java
@@ -1,13 +1,20 @@
 package com.example.homeplate;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.fragment.app.Fragment;
 
 public class ProfileFragment extends Fragment {
+
+    private TextView nameText;
+    private Button signOutButton;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_profile, container, false);
@@ -19,10 +26,21 @@ public class ProfileFragment extends Fragment {
     private void setValues(View view)
     {
 
+        //TextView
+        nameText = view.findViewById(R.id.profileName);
+
+        //Buttons
+        signOutButton = view.findViewById(R.id.signOutButtonProfile);
     }
 
     private void interact()
     {
-
+        signOutButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getActivity(), MainActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
