1fc0bdbc258df05df151f1bf37e52fdaf4e6b21e
Corbin <cgraham1@iastate.edu>
2021-12-03 00:06:56 -0600

    Designed User Chat Page

--- Frontend/Controller Design/HomePlate/app/src/main/java/com/example/homeplate/UserChatFragment.java
+++ Frontend/Controller Design/HomePlate/app/src/main/java/com/example/homeplate/UserChatFragment.java
@@ -4,16 +4,57 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.fragment.app.Fragment;
 
-/** Layout for the chat activity
+/**
+ * Layout for Individual User Chat
  * @author Corbin
  */
 public class UserChatFragment extends Fragment {
+
+    // Local Fields
+    TextView chatName;
+    TextView inputMessage;
+    Button sendButton;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_userchat, container, false);
+        View view = inflater.inflate(R.layout.fragment_userchat, container, false);
+
+        setValues(view);
+        interact();
+
+        return view;
     }
 
+    /**
+     * Initialize Fields
+     * and Values
+     * @param view Current View (Fragment)
+     */
+    private void setValues(View view)
+    {
+        // TextView
+        chatName = view.findViewById(R.id.chatName);
+
+        // TextBox
+        inputMessage = view.findViewById(R.id.inputMessage);
+
+        // Button
+        sendButton = view.findViewById(R.id.sendButton);
+
+        // Initialize
+    }
+
+    /**
+     * Wait for Interaction
+     * then Handle interaction
+     */
+    private void interact()
+    {
+
+    }
 }
